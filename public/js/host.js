3176209644b9fe91edf614d6a9653c153c04268b
Aiden Bai <mail.contraband@gmail.com>
2021-02-13 00:03:18 -0800

    gamer

--- public/js/host.js
+++ public/js/host.js
@@ -1,48 +1,79 @@
-// (() => {
-const app = Lucia.component({
-  id,
-  teams: [],
-  leaderboard: [],
-  entries: [],
-  timer: 15,
-  countdown: `15:00`,
-  score(num, i) {
-    const { teamName } = this.entries[i];
-    const team = this.teams.filter((t) => t[0] === teamName);
-    if (num === 1) {
-      socket.emit('score', { id, teamName: teamName, score: ++team[1] });
-    }
-    this.timer = 15;
-    this.entries.splice(i, 1);
-  },
-});
-app.mount('#app');
+let globalApp;
 
-const socket = io();
-socket.emit('create', id);
+(async () => {
+  const app = Lucia.component({
+    id,
+    leaderboard: [],
+    entries: [],
+    timer: -1,
+    score(num, i) {
+      const { teamName } = this.entries[i];
 
-socket.once('create', (success) => {
-  if (!success) location = `/`;
-});
+      let score;
 
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+      for (let i = 0; i < this.leaderboard.length; i++) {
+        if (this.leaderboard[i][0] === teamName) {
+          this.leaderboard[i][1] = 1;
+          console.log(this.leaderboard[i][1]);
+          score = i;
+          app.render();
+          break;
+        }
+      }
 
-socket.on('buzz', async (teamName) => {
-  if (!app.state.entries.some((entry) => entry.teamName === teamName)) {
-    app.state.entries.push({ teamName, date: new Date().toLocaleTimeString() });
-    while (app.state.timer > 0) {
-      app.state.timer--;
-      await delay(1000);
-    }
+      console.log(this.leaderboard);
+
+      // this.leaderboard.forEach(i => {
+      //   if(i.teamName === teamName){
+      //     score = ++i.score;
+      //     // wait why is this `= ++team.score?`
+      //   }
+      // })
+
+      this.timer = this.entries.length > 1 ? 15 : -1;
+
+      if (num === 1) {
+        socket.emit('score', { id, teamName: teamName, score });
+        console.log(score);
+        this.entries = [];
+        this.timer = -1;
+      } else {
+        this.entries.splice(i, 1);
+      }
+    },
+  });
+  app.mount('#app');
+  globalApp = app;
+
+  const socket = io();
+  socket.emit('create', id);
+
+  socket.once('create', (success) => {
+    if (!success) location = `/`;
+  });
+
+  function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
-});
 
-socket.on('join', (teamName) => {
-  if (!app.state.leaderboard.some((team) => team[0] === teamName)) {
-    app.state.leaderboard.push([teamName, 0]);
-  }
-});
+  socket.on('buzz', async (teamName) => {
+    if (!app.state.entries.some((entry) => entry.teamName === teamName)) {
+      app.state.entries.push({ teamName, date: new Date().toLocaleTimeString() });
+      if (app.state.entries.length === 1) {
+        app.state.timer = 15;
 
-// })();
+        while (app.state.timer > 0 && app.state.entries.length > 0) {
+          if (app.state.timer === 15) await delay(100);
+          app.state.timer--;
+          await delay(1000);
+        }
+      }
+    }
+  });
+
+  socket.on('join', (teamName) => {
+    if (!app.state.leaderboard.some((team) => team[0] === teamName)) {
+      app.state.leaderboard.push([teamName, 0]);
+    }
+  });
+})();
