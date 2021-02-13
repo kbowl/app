b34ef45f205a0bd822468d7cc6fdbbe26c3cb47d
tokq <melindachang.hy@gmail.com>
2021-02-12 16:14:01 -0800

    fixed views, head partial

--- src/routes/normal.ts
+++ src/routes/normal.ts
@@ -6,7 +6,17 @@ export default {
     const router = new Router();
 
     router.get('/', (_req, res) => {
-      res.render('index');
+      res.render('index',{ title: 'Home' });
+    });
+
+    router.get('/host', (_req, res) => {
+      res.render('host', { title: 'Host' });
+    });
+
+    router.get('/team', (_req, res) => {
+      res.render('team', {
+        title: 'Team'
+      });
     });
 
     return router;
